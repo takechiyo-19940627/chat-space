f3f5ca959ed6c6e88e75a678826aad86706b95e5
田中健洋 <take.19940627@gmail.com>
2019-04-28 18:07:36 +0900

    コミット
    コミット

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-=======
->>>>>>> ab240d3a1e87a4f2c9e4dd3fcae5bfdbe1e76130
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
