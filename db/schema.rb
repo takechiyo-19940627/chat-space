5c87d415875d1a1ff40f5d769775019a983c9d14
田中健洋 <take.19940627@gmail.com>
2019-04-30 19:06:09 +0900

    # WHAT ・メッセージ送信機能の実装開始 ・messageモデルの作成 ・groupとmessageのアソシエーションの構築 ・userをmessageのアソシエーションの構築　
    # WHY
    ・サービス必須機能のため

--- db/schema.rb
+++ db/schema.rb
@@ -28,9 +28,14 @@ ActiveRecord::Schema.define(version: 20190429082217) do
   end
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.text     "text",       limit: 65535
+    t.text     "content",    limit: 65535
+    t.string   "image"
+    t.integer  "user_id",                  null: false
+    t.integer  "group_id",                 null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
+    t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -49,4 +54,6 @@ ActiveRecord::Schema.define(version: 20190429082217) do
 
   add_foreign_key "group_users", "groups"
   add_foreign_key "group_users", "users"
+  add_foreign_key "messages", "groups"
+  add_foreign_key "messages", "users"
 end
