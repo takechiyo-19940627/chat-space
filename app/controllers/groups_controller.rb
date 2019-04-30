83ae906bb66a7741bb082df00877506695a49a1b
田中健洋 <take.19940627@gmail.com>
2019-04-30 13:15:55 +0900

    ・Viewの切り分け ・サイドバーのscssの調整

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,9 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
+  def index
+  end
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -7,7 +12,7 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      redirect_to root_url, notice: 'グループを作成しました'
+      redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
     end
@@ -17,11 +22,20 @@ class GroupsController < ApplicationController
   end
 
   def update
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
     def group_params
-      params.require(:group).permit(:name, { :user_id => []})
+      params.require(:group).permit(:name, { :user_id => [] })
+    end
+
+    def set_group
+      @group = Group.find(params[:id])
     end
 
 end
