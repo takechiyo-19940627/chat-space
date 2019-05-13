ef2678496366d5619fa7b2835cf6081410d26a36
田中健洋 <take.19940627@gmail.com>
2019-05-14 00:43:33 +0900

    search.js微修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,14 +3,18 @@ $(function () {
     function appendUser(user) {
         var html = '<div class="chat-group-user clearfix">' +
                         '<p class="chat-group-user__name">ユーザ名</p>' +
-                        '<div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザのid" data-user-name="ユーザ名">追加</div>' +
+                        '<div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">' +
+                            '追加' +
+                        '</div>' +
                    '</div>'
-        search_list.append(htmll)
+        search_list.append(html).data('user-id', 'user-name')
     }
 
     function appendErrMsgToHTML(msg) {
         var html = '<div class="chat-group-user clearfix">' +
-                        '<p class="chat-group-user__name"> ${msg} </p>' +
+                        '<p>' +
+                            `${ msg }` +
+                        '</p>' +
                    '</div>'
         search_list.append(html);
     }
@@ -26,7 +30,7 @@ $(function () {
         })
         .done(function (users) {
           $("#user-search-result").empty();
-          if(users.length !== 0) {
+          if (users.length !== 0) {
               users.forEach(function (user) {
                   appendUser(user);
               });
