3590214b7160e391c6c9630132c87ddd26719504
田中健洋 <take.19940627@gmail.com>
2019-05-14 23:04:34 +0900

    user.nameを検索候補に表示する

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,12 +2,14 @@ $(function () {
     var search_list = $("#user-search-result");
     function appendUser(user) {
         var html = '<div class="chat-group-user clearfix">' +
-                        '<p class="chat-group-user__name">ユーザ名</p>' +
+                        '<p class="chat-group-user__name">' +
+                            `${ user.name }` +
+                        '</p>' +
                         '<div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">' +
                             '追加' +
                         '</div>' +
                    '</div>'
-        search_list.append(html).data('user-id', 'user-name')
+        search_list.append(html)
     }
 
     function appendErrMsgToHTML(msg) {
