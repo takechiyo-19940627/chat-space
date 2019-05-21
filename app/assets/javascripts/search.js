1dc289281c2a8e1424743e27db7a810d27d840f2
田中健洋 <take.19940627@gmail.com>
2019-05-22 00:13:44 +0900

    ・生成するhtmlの記法をテンプレートリテラル記法に修正 ・グループ作成フォームの検索バーのタグをinputタグに修正

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,71 +1,79 @@
 $(function () {
     var search_list = $('#user-search-result');
     function appendUser(user) {
-        var html = '<div class="chat-group-user clearfix">' +
-                        '<p class="chat-group-user__name">' +
-                            `${ user.name }` +
-                        '</p>' +
-                        '<div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">' +
-                            '追加' +
-                        '</div>' +
-                   '</div>'
+        var html = `<div class="chat-group-user clearfix">
+                         <p class="chat-group-user__name">
+                            ${ user.name }
+                         </p> 
+                         <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">
+                            追加
+                         </a>
+                   </div>`
         search_list.append(html);
-        $('.user-search-add').on('click', function () {
-            var user_id = $('this').data("user-id");
-            var user_name = $('this').data("user-name");
-            appendUserToGroup(user);
-        });
-    }
-
-    var group_user_list = $('#chat-group-users');
-    function appendUserToGroup(user) {
-        var html = '<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">' +
-                        '<input name="group[user_ids][]" type="hidden" value="${ user.id }">' +
-                        '<p class="chat-group-user__name">' +
-                            `${ user.name}` +
-                        '</p>' +
-                        '<div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">' +
-                            '削除' +
-                        '</div>' +
-                   '</div>'
-        group_user_list.append(html);
-        $('.chat-group-user').on('click', function () {
-            $(this).remove();
-        })
     }
 
     function appendErrMsgToHTML(msg) {
         var html = '<div class="chat-group-user clearfix">' +
-                        '<p>' +
-                            `${ msg }` +
-                        '</p>' +
-                   '</div>'
+            '<p>' +
+            `${ msg }` +
+            '</p>' +
+            '</div>'
         search_list.append(html);
+    }
+
+    var group_user_list = $('#chat-group-users');
+    function appendUserToGroup(group_user) {
+        var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-${ group_user.id }">
+                         <input name="group[user_ids][]" type="hidden" value="${ group_user.id }">
+                         <p class="chat-group-user__name">
+                            ${ group_user.name }
+                         </p>
+                         <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">
+                            削除
+                        </a>
+                   </div>`
+        console.log(html);
+        group_user_list.append(html);
     }
 
     $('#user-search-field').on('keyup', function () {
         var input = $('#user-search-field').val();
-
-        $.ajax({
-            type: 'GET',
-            url: '/users/search',
-            data: { name: input },
-            dataType: 'json'
-        })
-        .done(function (users) {
-          $('#user-search-result').empty();
-          if (users.length !== 0) {
-              users.forEach(function (user) {
-                  appendUser(user);
-              });
-          }
-          else {
-              appendErrMsgToHTML("一致するユーザはいません");
-          }
-        })
-        .fail(function () {
-            alert('ユーザの検索に失敗しました')
-        })
+        if (input.length) {
+            $.ajax({
+                type: 'GET',
+                url: '/users/search',
+                data: { group_users: input },
+                dataType: 'json'
+            })
+                .done(function (users) {
+                    $('#user-search-result').empty();
+                    if (users.length !== 0) {
+                        users.forEach(function (user) {
+                            appendUser(user);
+                        });
+                    }
+                    else {
+                        appendErrMsgToHTML("一致するユーザはいません");
+                    }
+                })
+                .fail(function () {
+                    alert('ユーザの検索に失敗しました')
+                })
+        } else {
+            search_list.empty();
+        }
     });
+
+    $(document).on('click', '.user-search-add.chat-group-user__btn.chat-group-user__btn--add', function () {
+        var group_user = {};
+        group_user.id = $(this).attr("data-user-id");
+        group_user.name = $(this).attr("data-user-name");
+        $(this).parent().remove();
+        appendUserToGroup(group_user);
+    });
+
+    $(document).on("click", ".user-search-remove.chat-group-user__btn.chat-group-user__btn--remove.js-remove-btn", function () {
+        $(this).parent().remove()
+    })
 
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   end
 
   def search
-    @users = User.where('name LIKE(?)', "%#{params[:name]}%")
+    @users = User.where('name LIKE(?)', "%#{params[:group_users]}%")
     respond_to do |format|
       format.html
       format.json
